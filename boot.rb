7d52d343e677bcfd994482e8232a6f2664bbb61d
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-18 17:26:52 -0500

    Interrogate version of Ruby running

--- boot.rb
+++ boot.rb
@@ -6,4 +6,6 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), '/lib')
 ENV["REPO_PATH"] = File.join(File.expand_path(File.dirname(__FILE__)), '/repo')
 
 # gitolite-admin master path
-ENV["GLA_MASTER_PATH"] = ""
+ENV["GLA_MASTER_PATH"] = "gitolite:gitolite-admin"
+
+puts RUBY_VERSION
