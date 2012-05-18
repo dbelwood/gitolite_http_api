ff2bb3342b5c699f12d4306c2b8710591646fbb0
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-18 11:19:58 -0500

    Change REPO_ROOT to REPO_PATH as this is the correct variable

--- spec/shared_context.rb
+++ spec/shared_context.rb
@@ -1,15 +1,15 @@
 shared_context "Admin repo handling" do
-	OLD_REPO_ROOT ||= ENV["REPO_ROOT"]
+	OLD_REPO_PATH ||= ENV["REPO_PATH"]
 
 	before(:each) do
-		ENV["REPO_ROOT"] = File.join(File.expand_path(File.dirname(__FILE__)), '../test_repo')
-		FileUtils.mkdir_p "#{ENV["REPO_ROOT"]}/gitolite-admin"
-		Gitolite::GitoliteAdmin.bootstrap("#{ENV["REPO_ROOT"]}/gitolite-admin", {:user => "admin", :perm => "RW"})
+		ENV["REPO_PATH"] = File.join(File.expand_path(File.dirname(__FILE__)), '../test_repo')
+		FileUtils.mkdir_p "#{ENV["REPO_PATH"]}/gitolite-admin"
+		Gitolite::GitoliteAdmin.bootstrap("#{ENV["REPO_PATH"]}/gitolite-admin", {:user => "admin", :perm => "RW"})
 	end
 
 	after(:each) do
-		FileUtils.rm_rf("#{ENV["REPO_ROOT"]}/")
-		ENV["REPO_ROOT"] = OLD_REPO_ROOT
+		FileUtils.rm_rf("#{ENV["REPO_PATH"]}/")
+		ENV["REPO_PATH"] = OLD_REPO_PATH
 	end
 
 	def app
