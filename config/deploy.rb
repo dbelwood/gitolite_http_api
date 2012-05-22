2aa0307611c478992a8caaf7424edd4758a940ea
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-22 12:56:53 -0500

    Add git test server key to ssh keys and trigger repo creation after symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :group, user
 set :runner, user
  
 set :host, "api@ec2-184-73-92-234.compute-1.amazonaws.com" # We need to be able to SSH to that box as this user.
-set :ssh_options, {:user => "api", :keys => [File.join(ENV["HOME"], "pems", "MMAdmin.pem"), File.join(ENV["HOME"], ".ssh", "gitolite_http_api_deploy")], :forward_agent => true}
+set :ssh_options, {:user => "api", :keys => [File.join(ENV["HOME"], "pems", "MMAdmin.pem"), File.join(ENV["HOME"], ".ssh", "gitolite_http_api_deploy"), File.join(ENV["HOME"], ".ssh", "server")], :forward_agent => true}
 role :web, host
 role :app, host
  
@@ -31,9 +31,10 @@ set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
  
 # Unicorn control tasks
 namespace :deploy do
-  task :migrate do
+  task :init_repo do
   	run "cd #{deploy_to}/current && bundle exec rake setup"
   end
+  after "deploy:create_symlink", "deploy:init_repo"
   task :restart do
     run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
