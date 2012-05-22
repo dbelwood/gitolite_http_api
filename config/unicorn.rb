54128f8b5a44c28981ea6d2edcd3502c861a36dd
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-22 16:01:25 -0500

    Decrease worker count to get past this issue

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ err_log = "#{root}/log/unicorn_error.log"
 old_pid = pid_file + '.oldbin'
  
 timeout 30
-worker_processes 2 # increase or decrease
+worker_processes 1 # increase or decrease
 #listen socket_file, :backlog => 1024
 listen ENV['PORT'], :backlog => 1024
  
