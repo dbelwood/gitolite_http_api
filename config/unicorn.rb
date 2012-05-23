6cd58e51f8bb5fa06691914bcfe3d1045b92081e
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-22 21:06:26 -0500

    Listen on a socket as we are now fronted by nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,9 +9,9 @@ old_pid = pid_file + '.oldbin'
  
 timeout 30
 worker_processes 2 # increase or decrease
-#listen socket_file, :backlog => 1024
+listen socket_file, :backlog => 1024
 #listen ENV['PORT'], :backlog => 1024
-listen '0.0.0.0:8443', :backlog => 1024
+#listen '0.0.0.0:8443', :backlog => 1024
  
 pid pid_file
 stderr_path err_log
